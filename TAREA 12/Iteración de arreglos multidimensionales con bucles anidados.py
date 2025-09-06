72454b2cd6ece1756e502638b892221525c010f0
Marcelo Ivan Ortiz Gallegos <mi.ortizg@uea.edu.ec>
2025-09-06 17:18:15 -0500

    Tarea semana 12

--- TAREA 12/Iteración de arreglos multidimensionales con bucles anidados.py
+++ TAREA 12/Iteración de arreglos multidimensionales con bucles anidados.py
@@ -1,21 +1,27 @@
 # Crear una matriz 3D para almacenar datos de temperaturas
-# Primera dimensión: Ciudades (3 ciudades)
-# Segunda dimensión: Semanas (4 semanas)
-# Tercera dimensión: Días de la semana (7 días)
+
+# Ciudades, semanas y dias
+ciudades = ["Machala", "Portoviejo", "Ambato"]
+semanas = 4
+dias = ["lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+
+# Matriz 3D con valores de ejemplo
+# temperaturas [ciudad][semana][dia]
+
 temperaturas = [
-    [   # Ciudad Machala
+    [   # Machala
         [ 77, 81, 80, 78, 84, 87, 91], # Semana 1
         [ 78, 79, 84, 85, 82, 87, 90], # Semana 2
         [ 79, 83, 81, 85, 84, 93, 94], # Semana 3
         [ 76, 77, 81, 78, 85, 88, 92]  # Semana 4
     ],
-    [   # Ciudad Portoviejo
+    [   # Portoviejo
         [ 75, 60, 69, 80, 70, 71, 78], # Semana 1
         [ 72, 65, 69, 78, 71, 76, 85], # Semana 2
         [ 82, 66, 67, 71, 77, 73, 85], # Semana 3
         [ 74, 87, 68, 73, 75, 71, 84]  # Semana 4
     ],
-    [   # Ciudad Ambato
+    [   # Ambato
         [ 90, 82, 95, 92, 89, 86, 83], # Semana 1
         [ 81, 92, 95, 91, 88, 85, 84], # Semana 2
         [ 91, 93, 95, 92, 89, 86, 83], # Semana 3
@@ -23,14 +29,13 @@ temperaturas = [
     ]
 ]
 
-# Calcular el promedio de temperaturas para cada ciudad y semana
+# Calcular el promedios semanales por ciudad
 for i, ciudad in enumerate(ciudades):
-    print(f"\nCiudad: {ciudad}")
-    for semana_in range(semanas):
+    print(f"\nciudad: {ciudad}")
+    for semana in range(semanas):
         suma= 0
         for dia in range(len(dias)):
             suma += temperaturas[i][semana][dia]
         promedio = suma / len(dias)
-        print(f" Semana {semana +1}: Promedio = {promedio: .2f} ºC")
-
+        print(f" Semana {semana +1}: Promedio = {promedio:.2f} ºC")
 
